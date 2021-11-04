c6415544b78f898dc10e4419967d89d59fc71038
marvinlara41 <laramarvin105@gmail.com>
2021-11-03 20:45:19 -0400

    added layout

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let currentscorePlayer0 = document.querySelector(".current-score-player0");
+let currentscorePlayer1 = document.querySelector(".current-score-player1");
+const switchPlayer = document.querySelector(".active");
+
+const rollDice = document.querySelector(".rollDice");
+
+rollDice.addEventListener("click", function () {
+  console.log("working");
+});
