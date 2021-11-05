427192f74d0cbd6657f795a5e5ccab2ce3de6022
marvinlara41 <laramarvin105@gmail.com>
2021-11-04 21:30:59 -0400

    added random dice function

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 "use strict";
 
-let currentscorePlayer0 = document.querySelector(".current-score-player0");
-let currentscorePlayer1 = document.querySelector(".current-score-player1");
 const switchPlayer = document.querySelector(".active");
-
 const rollDice = document.querySelector(".rollDice");
+let revealDice = document.querySelector(".dice-number");
+let playing = true;
+let players = [0, 0];
+let currentScore = 0;
 
 rollDice.addEventListener("click", function () {
-  console.log("working");
+  if (playing) {
+    let diceNumber = Math.trunc(Math.random() * 6 + 1);
+
+    revealDice.src = `dice-${diceNumber}.png`;
+
+    revealDice.classList.remove("hiden");
+
+    currentScore += diceNumber;
+  }
 });
